e6ea7d5214dd72dffd723fa5df9ee3985e4550e1
Leonardo <leonardosarto1@hotmail.com>
2022-01-16 18:20:28 -0300

    Código do Guidetti

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import products.productsCreation as cadastro
 print("--------Menu temporário--------")
 print("1. Criar um arquivo")
 print("2. Editar um arquivo")
-print("3. Ler um arquivo")
+print("3. Consultar um arquivo")
 print("4. Excluir um arquivo")
 print("5. Sair do programa")
+print("------------------------")
 
-menu = int(input("Digite a opção"))
+menu = int(input("\nDigite a opção: "))
 if menu == 1:
-    cadastro.criarItem
+    produto = input("\nDigite o nome do arquivo para criar: ")
+    cadastro.criarItem(produto)
 elif menu == 2:
-    produto = input("Digite o nome do arquivo para editar")
+    produto = input("\nDigite o nome do arquivo para editar: ")
     cadastro.editarItem(produto)
 elif menu == 3:
-    cadastro.lerItem
+    produto = input("\nDigite o nome do arquivo para consultar: ")
+    cadastro.consultarItem(produto)
+elif menu == 4:
+    produto = input("\nDigite o nome do arquivo para excluir: ")
+    cadastro.deletarItem(produto)
+
+elif menu == 5:
+    exit()

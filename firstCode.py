296ff26375e405e816eeb84db8ecc1534981026a
Leonardo <leonardosarto1@hotmail.com>
2022-01-16 14:29:57 -0300

    Create firstCode.py

---
+++ firstCode.py
@@ -0,0 +1,10 @@
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close
+    except:
+        print("Houve um erro na criação do arquivo")
+    else:
+        print("Arquivo {nome} criado com sucesso")
+
+criarArquivo("Leonardo")

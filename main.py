5b7f070f2c977bc8056b37d1a12bc30313d50adf
Leonardo <leonardosarto1@hotmail.com>
2022-01-16 17:39:25 -0300

    Teste

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import products.productsCreation as cadastro
-import layout.interface as tela
 
-cadastro.criarArquivo("Teste criação de arquivo")
+print("--------Menu temporário--------")
+print("1. Criar um arquivo")
+print("2. Editar um arquivo")
+print("3. Ler um arquivo")
+print("4. Excluir um arquivo")
+print("5. Sair do programa")
+
+menu = int(input("Digite a opção"))
+if menu == 1:
+    cadastro.criarItem
+elif menu == 2:
+    produto = input("Digite o nome do arquivo para editar")
+    cadastro.editarItem(produto)
+elif menu == 3:
+    cadastro.lerItem
--- products/productsCreation.py
+++ products/productsCreation.py
@@ -1,8 +1,42 @@
-def criarArquivo(nome):
+
+
+def abrirItem(produto):
     try:
-        a = open(nome, 'wt+')
-        a.close
-    except:
-        print("Houve um erro na criação do arquivo")
+        a = open(produto, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
     else:
-        print("Arquivo {nome} criado com sucesso aaa")
+        return True
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def criarItem(produto):
+    try:
+        a = open(produto + ".txt", 'a')
+        a.close()
+    except:
+        print ('Erro')
+    else:
+        print ( produto + ' ' + 'Criado com sucesso')
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def lerItem(produto):
+    try:
+        a = open(produto + ".txt", 'rt')
+    except:
+        print ('Erro')
+    else:
+        print (a.read())
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def editarItem(produto):
+    try:
+        a = open(produto + ".txt", 'a')
+    except:
+        print ('Erro')
+    else:
+        descricao = input("Digite a descrição do produto")
+        a.writelines(descricao)
